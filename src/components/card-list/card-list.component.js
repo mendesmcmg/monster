7dd55263a4c4cfb36508e8b826d028014891e7c1
Clara Mendes <claraplanejamento@gmail.com>
2020-12-24 14:32:36 -0300

    feat: adds card component

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -1,5 +1,10 @@
 import './card-list.styles.css'
+import { Card } from "../card/card.component";
 
-export const CardList = (props) => {
-    return (<div className='card-list'> {props.children} </div>)
-}
+export const CardList = props => (
+  <div className='card-list'>
+    {props.monsters.map(monster => (
+      <Card key={monster.id} monster = {monster}/>
+    ))}
+  </div>
+)
